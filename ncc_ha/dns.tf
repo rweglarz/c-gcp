f1a368850d824456dca893cfe5ab71935cc8d4da
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2022-11-13 20:09:26 +0100

    feat: ncc_ha: add dns for the servers

--- ncc_ha/dns.tf
+++ ncc_ha/dns.tf
@@ -13,3 +13,25 @@ resource "google_dns_record_set" "ncc-fw" {
     google_compute_instance.fw[count.index].network_interface.1.access_config.0.nat_ip
   ]
 }
+
+resource "google_dns_record_set" "ncc-srv0" {
+  managed_zone = data.google_dns_managed_zone.gke.name
+  name = "ncc-srv0.${data.google_dns_managed_zone.gke.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  rrdatas = [
+    google_compute_instance.ncc-srv0.network_interface.0.access_config.0.nat_ip
+  ]
+}
+
+resource "google_dns_record_set" "ncc-srv1" {
+  managed_zone = data.google_dns_managed_zone.gke.name
+  name = "ncc-srv1.${data.google_dns_managed_zone.gke.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  rrdatas = [
+    google_compute_instance.ncc-srv1.network_interface.0.access_config.0.nat_ip
+  ]
+}
