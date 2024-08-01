00acff689d7150c1f59378a8f5de99b1256b2523
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2024-08-01 17:30:31 +0200

    chore: ha: roll up dns

--- ha/dns.tf
+++ ha/dns.tf
@@ -2,36 +2,20 @@ data "google_dns_managed_zone" "gke" {
   name     = var.dns_zone
 }
 
-resource "google_dns_record_set" "fw" {
-  count = 2
+resource "google_dns_record_set" "this" {
+  for_each = {
+    ha-srv0 = google_compute_instance.srv0.network_interface.0.access_config.0.nat_ip,
+    ha-srv1 = google_compute_instance.srv1.network_interface.0.access_config.0.nat_ip,
+    ha-fw0  = google_compute_instance.fw[0].network_interface.1.access_config.0.nat_ip,
+    ha-fw1  = google_compute_instance.fw[1].network_interface.1.access_config.0.nat_ip,
+  }
+
   managed_zone = data.google_dns_managed_zone.gke.name
-  name = "ha-fw${count.index}.${data.google_dns_managed_zone.gke.dns_name}"
+  name = "${each.key}.${data.google_dns_managed_zone.gke.dns_name}"
   type = "A"
-  ttl  = 300
+  ttl  = 120
 
   rrdatas = [
-    google_compute_instance.fw[count.index].network_interface.1.access_config.0.nat_ip
-  ]
-}
-
-resource "google_dns_record_set" "srv0" {
-  managed_zone = data.google_dns_managed_zone.gke.name
-  name = "ha-srv0.${data.google_dns_managed_zone.gke.dns_name}"
-  type = "A"
-  ttl  = 300
-
-  rrdatas = [
-    google_compute_instance.srv0.network_interface.0.access_config.0.nat_ip
-  ]
-}
-
-resource "google_dns_record_set" "srv1" {
-  managed_zone = data.google_dns_managed_zone.gke.name
-  name = "ha-srv1.${data.google_dns_managed_zone.gke.dns_name}"
-  type = "A"
-  ttl  = 300
-
-  rrdatas = [
-    google_compute_instance.srv1.network_interface.0.access_config.0.nat_ip
+    each.value
   ]
 }
