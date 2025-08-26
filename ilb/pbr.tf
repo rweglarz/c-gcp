62f854bbc72631bb864c83465a7c0fda559271a3
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2025-08-26 16:40:32 +0200

    feat: ilb: enable pbr also for peered vpcs

--- ilb/pbr.tf
+++ ilb/pbr.tf
@@ -18,9 +18,15 @@ resource "google_network_connectivity_policy_based_route" "a-fw" {
 }
 
 resource "google_network_connectivity_policy_based_route" "a" {
-  name = "${var.name}-private-a"
-  description = "workloads in private-a"
-  network = google_compute_network.private["a"].id
+  for_each = merge(
+    { 
+      private-a = google_compute_network.private["a"].id
+    },
+    { for k,v in google_compute_network.peer: k=>v.id if strcontains(k, "private-a") },
+  )
+  name = "${var.name}-${each.key}"
+  description = "workloads in ${each.key}"
+  network = each.value
   priority = 100
 
   filter {
@@ -31,6 +37,30 @@ resource "google_network_connectivity_policy_based_route" "a" {
   }
   next_hop_ilb_ip = google_compute_forwarding_rule.private["a"].ip_address
 }
+
+resource "google_network_connectivity_policy_based_route" "b" {
+  for_each = merge(
+    { 
+      private-b = google_compute_network.private["b"].id
+    },
+    { for k,v in google_compute_network.peer: k=>v.id if strcontains(k, "private-b") },
+  )
+
+  name = "${var.name}-${each.key}"
+  description = "workloads in ${each.key}"
+  network = each.value
+  priority = 100
+
+  filter {
+    protocol_version = "IPV4"
+    ip_protocol = "ALL"
+    src_range = "172.16.0.0/12"
+    dest_range = "172.16.0.0/12"
+  }
+  next_hop_ilb_ip = google_compute_forwarding_rule.private["b"].ip_address
+}
+
+
 
 resource "google_network_connectivity_policy_based_route" "privatevpn-aa--ab" {
   name = "${var.name}-privatevpn-aa-to-ab"
@@ -46,6 +76,7 @@ resource "google_network_connectivity_policy_based_route" "privatevpn-aa--ab" {
   }
   next_hop_ilb_ip = google_compute_forwarding_rule.private["a"].ip_address
 }
+
 resource "google_network_connectivity_policy_based_route" "privatevpn-ab--aa" {
   name = "${var.name}-privatevpn-ab-to-aa"
   description = "vpns in private-a"
