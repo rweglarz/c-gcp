dd3a842b3b7b2c942225a2ab11630646320c7528
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2026-01-29 18:00:55 +0100

    chore: gke: add default cidrs

--- gke/variables.tf
+++ gke/variables.tf
@@ -7,12 +7,16 @@ variable "region" {
 variable "name" {
   type = string
 }
+
 variable "cidr" {
-  type = string
+  type    = string
+  default = "172.30.0.0/16"
 }
 variable "m_cidr" {
-  type = string
+  type    = string
+  default = "172.29.254.240/28"
 }
+
 variable "mgmt_ips" {
   description = "List of IPs allowed for external access"
   type        = list(map(string))
