804b300511e70fa2283134abe799d29091934a2e
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2026-06-16 10:12:19 +0200

    feat: ilb-sdwan: add servers

--- ilb-sdwan/servers.tf
+++ ilb-sdwan/servers.tf
@@ -29,3 +29,34 @@ resource "google_compute_instance" "jumphost" {
     ]
   }
 }
+
+resource "google_compute_instance" "servers" {
+  for_each = {
+    workload-a = google_compute_subnetwork.workload["workload-a"].id
+    workload-b = google_compute_subnetwork.workload["workload-b"].id
+    remote     = google_compute_subnetwork.remote.id
+  }
+  name    = "${var.name}-srv-${each.key}"
+  machine_type = "f1-micro"
+  zone         = var.zone
+  # service_account {
+  #     email  = google_service_account.saj.email
+  #     scopes = ["cloud-platform"]
+  # }
+
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.ubuntu.id
+    }
+  }
+
+  network_interface {
+    subnetwork = each.value
+  }
+  lifecycle {
+    ignore_changes = [
+      boot_disk[0].initialize_params[0].image
+    ]
+  }
+  allow_stopping_for_update = true
+}
